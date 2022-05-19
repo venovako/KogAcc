b387b137ae77bc7e29696a7e50c34192c3503a0c
Vedran Novakovic <venovako@venovako.eu>
2022-05-19 18:48:35 +0200

    Clean up the code.

--- src/gksvd2.f90
+++ src/gksvd2.f90
@@ -57,19 +57,17 @@
   ELSE IF (B(1,1) .EQ. ZERO) THEN
      S(1) = ABS(B(2,1))
   ELSE ! full 1st column
-     ! if numerical compatibility with the vectorized algorithm is not
-     ! required, replace the remaining code in this branch by the line
-     ! S(1) = HYPOT(B(1,1), B(2,1))
-     X = ABS(B(1,1))
-     Y = ABS(B(2,1))
-     Z = MAX(X, Y)
-     IF (Z .EQ. ZERO) THEN
-        S(1) = ZERO
-     ELSE ! general case
-        X = MIN(X, Y) / Z
-        Y = X * X + ONE
-        S(1) = SQRT(Y) * Z
-     END IF
+     S(1) = HYPOT(B(1,1), B(2,1))
+     ! X = ABS(B(1,1))
+     ! Y = ABS(B(2,1))
+     ! Z = MAX(X, Y)
+     ! IF (Z .EQ. ZERO) THEN
+     !    S(1) = ZERO
+     ! ELSE ! general case
+     !    X = MIN(X, Y) / Z
+     !    Y = X * X + ONE
+     !    S(1) = SQRT(Y) * Z
+     ! END IF
   END IF
 
   ! compute the second column norm
@@ -78,34 +76,28 @@
   ELSE IF (B(2,2) .EQ. ZERO) THEN
      S(2) = ABS(B(1,2))
   ELSE ! full 2nd column
-     ! if numerical compatibility with the vectorized algorithm is not
-     ! required, replace the remaining code in this branch by the line
-     ! S(2) = HYPOT(B(1,2), B(2,2))
-     X = ABS(B(1,2))
-     Y = ABS(B(2,2))
-     Z = MAX(X, Y)
-     IF (Z .EQ. ZERO) THEN
-        S(2) = ZERO
-     ELSE ! general case
-        X = MIN(X, Y) / Z
-        Y = X * X + ONE
-        S(2) = SQRT(Y) * Z
-     END IF
+     S(2) = HYPOT(B(1,2), B(2,2))
+     ! X = ABS(B(1,2))
+     ! Y = ABS(B(2,2))
+     ! Z = MAX(X, Y)
+     ! IF (Z .EQ. ZERO) THEN
+     !    S(2) = ZERO
+     ! ELSE ! general case
+     !    X = MIN(X, Y) / Z
+     !    Y = X * X + ONE
+     !    S(2) = SQRT(Y) * Z
+     ! END IF
   END IF
 
   ! swap the columns if necessary
   IF (S(1) .LT. S(2)) THEN
-     Z = B(1,1)
+     Y = B(1,1)
      B(1,1) = B(1,2)
-     B(1,2) = Z
+     B(1,2) = Y
 
-     Z = B(2,1)
+     Y = B(2,1)
      B(2,1) = B(2,2)
-     B(2,2) = Z
-
-     Z = S(1)
-     S(1) = S(2)
-     S(2) = Z
+     B(2,2) = Y
 
      Z = V(1,1)
      V(1,1) = V(1,2)
@@ -114,41 +106,45 @@
      Z = V(2,1)
      V(2,1) = V(2,2)
      V(2,2) = Z
+
+     Y = S(1)
+     S(1) = S(2)
+     S(2) = Y
   END IF
 
   ! make B(1,1) non-negative
   IF (SIGN(ONE, B(1,1)) .NE. ONE) THEN
-     B(1,1) = -B(1,1)
-     B(1,2) = -B(1,2)
      U(1,1) = -U(1,1)
      U(1,2) = -U(1,2)
+     B(1,1) = -B(1,1)
+     B(1,2) = -B(1,2)
   END IF
 
   ! make B(2,1) non-negative
   IF (SIGN(ONE, B(2,1)) .NE. ONE) THEN
-     B(2,1) = -B(2,1)
-     B(2,2) = -B(2,2)
      U(2,1) = -U(2,1)
      U(2,2) = -U(2,2)
+     B(2,1) = -B(2,1)
+     B(2,2) = -B(2,2)
   END IF
 
   ! swap the rows if necessary
   IF (B(1,1) .LT. B(2,1)) THEN
-     Z = B(1,1)
-     B(1,1) = B(2,1)
-     B(2,1) = Z
-
      Z = U(1,1)
      U(1,1) = U(2,1)
      U(2,1) = Z
 
-     Z = B(1,2)
-     B(1,2) = B(2,2)
-     B(2,2) = Z
-
      Z = U(1,2)
      U(1,2) = U(2,2)
      U(2,2) = Z
+
+     X = B(1,1)
+     B(1,1) = B(2,1)
+     B(2,1) = X
+
+     X = B(1,2)
+     B(1,2) = B(2,2)
+     B(2,2) = X
   END IF
 
   ! compute the Givens rotation
@@ -182,10 +178,10 @@
 
   ! make B(2,2) non-negative
   IF (SIGN(ONE, B(2,2)) .NE. ONE) THEN
-     B(2,1) = -B(2,1)
-     B(2,2) = -B(2,2)
      U(2,1) = -U(2,1)
      U(2,2) = -U(2,2)
+     B(2,1) = -B(2,1)
+     B(2,2) = -B(2,2)
   END IF
 
   ! exit if B is a nul-matrix
