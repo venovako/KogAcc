d668c5f6ea7f206d149a20f6cf1b1f33eff06a1a
Vedran Novakovic <venovako@venovako.eu>
2022-07-24 22:27:54 +0200

    small updates

--- src/ifort.mk
+++ src/ifort.mk
@@ -9,7 +9,7 @@ endif # ?NDEBUG
 #-DUSE_IEEE_INTRINSIC
 FCFLAGS += -xHost -qopt-multi-version-aggressive -fPIC -fexceptions -fno-omit-frame-pointer -rdynamic -traceback -fp-model precise -fprotect-parens -fma -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -standard-semantics
 ifdef NDEBUG
-FCFLAGS += -qopt-report=5
+FCFLAGS += -qopt-report=5 -inline-level=2 -vec-threshold0
 else # !NDEBUG
 FCFLAGS += -debug emit_column -debug extended -debug inline-debug-info -debug pubnames -fp-stack-check
 endif # ?NDEBUG
--- src/ifx.mk
+++ src/ifx.mk
@@ -9,7 +9,7 @@ endif # ?NDEBUG
 #-DUSE_IEEE_INTRINSIC
 FCFLAGS += -xHost -fPIC -fexceptions -fno-omit-frame-pointer -rdynamic -traceback -fp-model precise -fprotect-parens -fma -no-ftz -standard-semantics -static-libgcc
 ifdef NDEBUG
-FCFLAGS += -qopt-report=3
+FCFLAGS += -qopt-report=3 -inline-level=2 -vec-threshold0
 else # !NDEBUG
 FCFLAGS += -debug emit_column -debug extended -debug inline-debug-info -debug pubnames
 endif # ?NDEBUG
