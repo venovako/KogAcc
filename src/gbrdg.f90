ce3557b1712b152bfeb8acd16bafc2f1a77b419a
Vedran Novakovic <venovako@venovako.eu>
2022-10-02 21:38:05 +0200

    a few updates

--- src/gbrdg.f90
+++ src/gbrdg.f90
@@ -1,4 +1,4 @@
-  ! This is the generic part of the OpenMP-parallel bordering routines.
+  ! This is the generic part of the sequential bordering routines.
   INFO = 0
   IF (LDG .LT. MAX(M, 0)) INFO = -4
   IF (N .LT. 0) INFO = -2
@@ -6,14 +6,11 @@
   IF (INFO .NE. 0) RETURN
   IF (M .EQ. 0) RETURN
   IF (N .EQ. M) RETURN
-  !$OMP PARALLEL DO DEFAULT(NONE) SHARED(M,N,G)
   DO J = 1, N
      DO I = N+1, M
         G(I,J) = ZERO
      END DO
   END DO
-  !$OMP END PARALLEL DO
-  !$OMP PARALLEL DO DEFAULT(NONE) SHARED(M,N,G)
   DO J = N+1, M
      DO I = 1, J-1
         G(I,J) = ZERO
@@ -23,4 +20,3 @@
         G(I,J) = ZERO
      END DO
   END DO
-  !$OMP END PARALLEL DO
---
+++ src/zbrdg.f90
@@ -0,0 +1,17 @@
+!>@brief \b ZBRDG borders a double precision complex square matrix of order \f$N\f$ to a matrix of order \f$M\ge N\f$.
+!!
+!!@param M [IN]; \f$M\f$ is the final matrix order.
+!!@param N [IN]; \f$N\f$ is the initial matrix order (at most \f$M\f$).
+!!@param G [OUT]; \f$G\f$ is a double precision complex square matrix that gets bordered, such that the \f$M-N\f$ appended rows and columns are set to zero, except on the diagonal, which is set to unity, while the first \f$N\f$ rows and columns of \f$G\f$ are not touched.
+!!@param LDG [IN]; the leading dimension of \f$G\f$.
+!!@param INFO [OUT]; zero on success or \f$-i\f$ if the \f$i\f$th argument had an illegal value.
+SUBROUTINE ZBRDG(M, N, G, LDG, INFO)
+  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: REAL64
+  IMPLICIT NONE
+  COMPLEX(KIND=REAL64), PARAMETER :: ZERO = (0.0_REAL64,0.0_REAL64), ONE = (1.0_REAL64,0.0_REAL64)
+  INTEGER, INTENT(IN) :: M, N, LDG
+  COMPLEX(KIND=REAL64), INTENT(OUT) :: G(LDG,M)
+  INTEGER, INTENT(OUT) :: INFO
+  INTEGER :: I, J
+  INCLUDE 'gbrdg.f90'
+END SUBROUTINE ZBRDG
