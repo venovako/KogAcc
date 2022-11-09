15ede2bdcdc7dcf074cffeb65fd8eb02fdb0c465
Vedran Novakovic <venovako@venovako.eu>
2022-11-09 20:12:54 +0100

    Enable optional usage of the correctly rounded routines from the CORE-MATH project.

--- src/x86_64.mk
+++ src/x86_64.mk
@@ -1,3 +1,8 @@
 ifeq ($(COMPILER),gfortran)
 FCFLAGS += -march=native
 endif # gfortran
+ifdef CR_MATH
+FCFLAGS += -DCR_MATH
+OBJS_CRF_MATH=$(CR_MATH)/src/binary32/hypot/hypotf.o
+OBJS_CRD_MATH=$(CR_MATH)/src/binary64/hypot/hypot.o
+endif # CR_MATH
