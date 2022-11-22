c062539d1138053eaa48a42b27151b95b4c190eb
Vedran Novakovic <venovako@venovako.eu>
2022-11-23 00:57:32 +0100

    further updates

--- src/Linux.mk
+++ src/Linux.mk
@@ -3,9 +3,17 @@ ifeq ($(ARCH),x86_64)
 ifeq ($(COMPILER),gfortran)
 LAPACK=-L$(MKLROOT)/lib/intel64 -Wl,-rpath=$(MKLROOT)/lib -lmkl_gf_$(ABI) -lmkl_sequential -lmkl_core -lpthread -lm -ldl
 else # other compilers
+ifeq ($(COMPILER),nvfortran)
+LAPACK=-llapack_$(ABI) -lblas_$(ABI)
+else # ifort/ifx
 LAPACK=-L$(MKLROOT)/lib/intel64 -Wl,-rpath=$(MKLROOT)/lib -lmkl_intel_$(ABI) -lmkl_sequential -lmkl_core -lpthread -lm -ldl
+endif # ?nvfortran
 endif # ?gfortran
 else # !x86_64
+ifeq ($(COMPILER),nvfortran)
+LAPACK=-llapack_$(ABI) -lblas_$(ABI)
+else # !nvfortran
 LAPACK=-L$(HOME)/lapack_$(ABI) -ltmglib -llapack -lrefblas -lm
+endif # ?nvfortran
 endif # ?x86_64
 endif # !LAPACK
