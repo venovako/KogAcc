55c30f9ea64e864a9be0e2d4e7cc325ccafb5f25
Vedran Novakovic <venovako@venovako.eu>
2022-11-23 01:27:59 +0100

    further updates

--- src/xlf.mk
+++ src/xlf.mk
@@ -6,7 +6,7 @@ FCFLAGS=-O$(NDEBUG)
 else # !NDEBUG
 FCFLAGS=-O0
 endif # ?NDEBUG
-FCFLAGS += -qinit=f90ptr -qsclk=micro -qpic -qsaveopt -qarch=auto -qsimd=auto -qsmp=omp -qstacktemp=-1 -qstrict -qstrict_induction
+FCFLAGS += -qinit=f90ptr -qsclk=micro -qpic -qsaveopt -qarch=auto -qsimd=auto -qnosave -qstacktemp=-1 -qstrict -qstrict_induction #-qsmp=omp
 ifndef NDEBUG
 FCFLAGS += -qcheck -qdbg
 endif # !NDEBUG
