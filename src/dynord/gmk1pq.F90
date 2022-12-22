061b12c79ecf593cb8c9203105c8a1369173ec2d
Vedran Novakovic <venovako@venovako.eu>
2022-12-22 21:37:21 +0100

    further updates

--- src/dynord/gmk1pq.F90
+++ src/dynord/gmk1pq.F90
@@ -12,25 +12,54 @@
      W(1) = ZERO
      RETURN
   END IF
+
   M = N * (N - 1)
   M_2 = M / 2
+  J = 2 * M
 
-  CALL ABSG(N, N, G, LDG, W, N, INFO)
-  IF (INFO .NE. 0) RETURN
-  J = N * N
-  DO I = 1, J, N+1
-     W(I) = ZERO
-  END DO
-  W(J) = NORM2(W)
-  CALL MKWPQ(N, G, LDG, W, O, INFO)
-  IF (INFO .LE. 0) RETURN
-  L = MIN(L, INFO)
+  ! W = | G |
+  CALL ABSG(N, N, G, LDG, W, N, O(J))
+  IF (O(J) .NE. 0) THEN
+     INFO = -3
+     RETURN
+  END IF
+  ! (W,P,Q) = (W(1:M_2),O(1:M_2),O(M_2+1:M))
+  CALL MKWPQ(N, G, LDG, W, O, O(J))
+  IF (O(J) .EQ. 0) THEN
+     INFO = 0
+     W(M_2 + 1) = ZERO
+     RETURN
+  END IF
+  IF (O(J) .LT. 0) THEN
+     INFO = -5
+     RETURN
+  END IF
+  I = O(J)
+  L = MIN(L, I)
+  ! sort (W,P,Q)
   CALL PQSRT(PQCMP, M_2, W, O, O(M_2+1), W(M_2+1), O(M+1), O(M+M_2+1), INFO)
-  IF (INFO .LT. 0) RETURN
+  O(J) = INFO
+  IF (INFO .LT. 0) THEN
+     INFO = -6
+     RETURN
+  END IF
 
-  I = J
+  ! W(J) = || off(G) ||_F
   J = M_2 + 1
-  W(J) = W(I)
+  W(J) = ZERO
+  DO WHILE (I .GE. 1)
+     IF (W(I) .GT. ZERO) THEN
+        W(J) = W(I)
+        I = I - 1
+        EXIT
+     END IF
+     I = I - 1
+  END DO
+  DO WHILE (I .GE. 1)
+     W(J) = CR_HYPOT(W(J), W(I))
+     I = I - 1
+  END DO
+
 #ifndef NDEBUG
   DO I = J+1, M
      W(I) = ZERO
@@ -44,7 +73,6 @@
      O(I) = 0
   END DO
 #endif
-  O(2*M) = INFO
 
   I = 1
   INFO = 0
