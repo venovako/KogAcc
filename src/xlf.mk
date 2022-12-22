a02e76fa56da84922070eb6bef129c39eadfd0c7
Vedran Novakovic <venovako@venovako.eu>
2022-12-23 00:44:19 +0100

    further updates

--- src/xlf.mk
+++ src/xlf.mk
@@ -6,10 +6,8 @@ FCFLAGS=-O$(NDEBUG)
 else # !NDEBUG
 FCFLAGS=-O0
 endif # ?NDEBUG
-# with XLF 17.1.1.0 the OpenMP parts fail to compile
-#FCFLAGS += -qinit=f90ptr -qsclk=micro -qpic -qsaveopt -qarch=auto -qsimd=auto -qnosave -qstacktemp=-1 -qstrict -qstrict_induction
-# XLF 16.1.1.3 does not recognize pwr10
-FCFLAGS += -qinit=f90ptr -qsclk=micro -qpic -qsaveopt -qarch=pwr9 -qsimd=auto -qsmp=omp -qstacktemp=-1 -qstrict -qstrict_induction
+# XLF 16.1.1.13
+FCFLAGS += -qinit=f90ptr -qsclk=micro -qpic -qsaveopt -qarch=auto -qsimd=auto -qsmp=omp -qstacktemp=-1 -qstrict -qstrict_induction
 ifndef NDEBUG
 FCFLAGS += -qcheck -qdbg
 endif # !NDEBUG
