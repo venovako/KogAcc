dc6f4c12d9a9ccc0b5d7a2d6339dfdac7e9f6f8c
Vedran Novakovic <venovako@venovako.eu>
2024-02-19 00:35:43 +0100

    further updates

--- etc/cfma.F90
+++ etc/cfma.F90
@@ -1,3 +1,4 @@
+! after CUDA's complex FMA from cuComplex.h
 #ifdef USE_IEEE_INTRINSIC
 #define CFMA(A,B,C) CMPLX(IEEE_FMA(REAL(A),REAL(B),IEEE_FMA(-AIMAG(A),AIMAG(B),REAL(C))),IEEE_FMA(REAL(A),AIMAG(B),IEEE_FMA(AIMAG(A),REAL(B),AIMAG(C))),K)
 #else
