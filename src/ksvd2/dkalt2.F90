349b34ada6d4bb6d9178ad1204a0ce5b3e942b91
Vedran Novakovic <venovako@venovako.eu>
2024-02-24 22:23:13 +0100

    further updates

--- src/ksvd2/dkalt2.F90
+++ src/ksvd2/dkalt2.F90
@@ -6,71 +6,25 @@
 !!@param S [OUT]; S' is a double precision array with two elements, s_{11}' and s_{22}', both non-negative and finite.
 !!@param INFO [INOUT]; do not set to anything but zero on input unless the effects are understood; on output, the scaling parameter s such that 2^{-s} S' = S.
 !!If G has a non-finite element, then s=-HUGE(s), U=V=I, and s_{11}'=s_{22}'=0.
-#ifdef NDEBUG
-PURE SUBROUTINE DKALT2(G, U, V, S, INFO)
-  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: REAL64
-#else
 SUBROUTINE DKALT2(G, U, V, S, INFO)
-  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: ERROR_UNIT, REAL64
-  !$ USE OMP_LIB
-#endif
-#ifdef USE_IEEE_INTRINSIC
-#if ((USE_IEEE_INTRINSIC & 12) == 0)
-#undef USE_IEEE_INTRINSIC
-#elif ((USE_IEEE_INTRINSIC & 12) == 4)
-  USE, INTRINSIC :: IEEE_ARITHMETIC, ONLY: IEEE_FMA
-#endif
-#endif
+  USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_int
+  USE, INTRINSIC :: ISO_FORTRAN_ENV, ONLY: REAL64
   IMPLICIT NONE
 
-#ifdef USE_IEEE_INTRINSIC
-#if ((USE_IEEE_INTRINSIC & 12) == 8)
-  INTERFACE
-#ifdef NDEBUG
-     PURE FUNCTION IEEE_FMA(X, Y, Z) BIND(C,NAME='fma')
-#else
-     FUNCTION IEEE_FMA(X, Y, Z) BIND(C,NAME='fma')
-#endif
-       USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_double
-       REAL(KIND=c_double), INTENT(IN), VALUE :: X, Y, Z
-       REAL(KIND=c_double) :: IEEE_FMA
-     END FUNCTION IEEE_FMA
-  END INTERFACE
-#endif
-#endif
-#ifdef CR_MATH
-  INTERFACE
-#ifdef NDEBUG
-     PURE FUNCTION CR_HYPOT(X, Y) BIND(C,NAME='cr_hypot')
-#else
-     FUNCTION CR_HYPOT(X, Y) BIND(C,NAME='cr_hypot')
-#endif
-       USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_double
-       REAL(KIND=c_double), INTENT(IN), VALUE :: X, Y
-       REAL(KIND=c_double) :: CR_HYPOT
-     END FUNCTION CR_HYPOT
-  END INTERFACE
-#else
-#define CR_HYPOT HYPOT
-#endif
-
-  INTEGER, PARAMETER :: K = REAL64, IERR = -HUGE(0)
-  REAL(KIND=K), PARAMETER :: ZERO = 0.0_K, ONE = 1.0_K, TWO = 2.0_K
-  REAL(KIND=K), PARAMETER :: H = HUGE(ZERO), ROOT2 = SQRT(TWO)
-#ifndef CR_MATH
-  REAL(KIND=K), PARAMETER :: ROOTH = SQRT(H)
-#endif
-
+  INTEGER, PARAMETER :: K = REAL64
   REAL(KIND=K), INTENT(IN) :: G(2,2)
   REAL(KIND=K), INTENT(OUT) :: U(2,2), V(2,2), S(2)
   INTEGER, INTENT(INOUT) :: INFO
+  INTEGER(c_int), EXTERNAL :: PVN_DLJSV2
 
-  REAL(KIND=K) :: A(2,2), B(2,2), T, X, Y, Z
-  REAL(KIND=K) :: TANG, SECG, TANF, SECF, COSF, SINF, TANP, SECP, COSP, SINP
-  INTEGER :: I, J, L
+  INTEGER(c_int) :: ES, KND
 
-#include "gkalt2.F90"
-#ifndef NDEBUG
-9 FORMAT(2(A,ES25.17E3))
-#endif
+  ES = INT(INFO, c_int)
+  KND = PVN_DLJSV2(G(1,1), G(2,1), G(1,2), G(2,2), &
+       U(1,1), U(2,1), U(1,2), U(2,2), V(1,1), V(2,1), V(1,2), V(2,2), S(1), S(2), ES)
+  IF (KND .LT. 0) THEN
+     INFO = -HUGE(INFO)
+  ELSE ! all OK
+     INFO = INT(ES)
+  END IF
 END SUBROUTINE DKALT2
