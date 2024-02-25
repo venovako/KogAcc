938197e2142c0be032ba94f4762e7f1055804c94
Vedran Novakovic <venovako@venovako.eu>
2024-02-25 04:39:21 +0100

    further updates

--- src/ifort.mk
+++ src/ifort.mk
@@ -1,3 +1,4 @@
+# ifort has been deprecated
 AR=xiar
 ARFLAGS=-qnoipo -lib rsv
 FC=$(COMPILER_PREFIX)ifort$(COMPILER_SUFFIX)
@@ -6,9 +7,9 @@ FCFLAGS=-O$(NDEBUG)
 else # !NDEBUG
 FCFLAGS=-O0 -g
 endif # ?NDEBUG
-FCFLAGS += -xHost -fPIC -fexceptions -fno-omit-frame-pointer -qopt-multi-version-aggressive -qopt-zmm-usage=high -fp-model precise -fma -fprotect-parens -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -qopenmp -qsimd-honor-fp-model -qsimd-serialize-fp-reduction -standard-semantics -traceback -vec-threshold0 -rdynamic
+FCFLAGS += -diag-disable=10397,10448 -xHost -fPIC -fexceptions -fno-omit-frame-pointer -qopt-multi-version-aggressive -qopt-zmm-usage=high -fp-model precise -fma -fprotect-parens -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -qopenmp -qsimd-honor-fp-model -qsimd-serialize-fp-reduction -standard-semantics -traceback -vec-threshold0 -rdynamic
 ifdef NDEBUG
-FCFLAGS += -qopt-report=5 -diag-disable=10397 -inline-level=2
+FCFLAGS += -qopt-report=5 -inline-level=2
 else # !NDEBUG
 FCFLAGS += -debug emit_column -debug extended -debug inline-debug-info -debug pubnames -check all -fp-stack-check
 endif # ?NDEBUG
