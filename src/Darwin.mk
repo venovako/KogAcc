9af4111443a8ae9ca81855bb5e2b621a6250ce1e
Vedran Novakovic <venovako@venovako.eu>
2024-03-07 03:44:29 +0100

    further updates

--- src/Darwin.mk
+++ src/Darwin.mk
@@ -1,7 +1,9 @@
-ifndef LAPACK
+ifdef LAPACK
+LAPACK_LIBS=-L$(LAPACK) -ltmglib -llapack -lrefblas -lm
+else # !LAPACK
 ifeq ($(ARCH),x86_64)
 LAPACK_LIBS=-L$(MKLROOT)/lib -Wl,-rpath,$(MKLROOT)/lib -lmkl_intel_$(ABI) -lmkl_sequential -lmkl_core -lpthread -lm -ldl
 else # !x86_64
-LAPACK_LIBS=-L$(LAPACK) -ltmglib -llapack -lrefblas -lm
+# TODO
 endif # ?x86_64
-endif # !LAPACK
+endif # ?LAPACK
--- src/Linux.mk
+++ src/Linux.mk
@@ -1,4 +1,6 @@
-ifndef LAPACK
+ifdef LAPACK
+LAPACK_LIBS=-L$(LAPACK) -ltmglib -llapack -lrefblas -lm
+else # !LAPACK
 ifeq ($(ARCH),x86_64)
 ifeq ($(COMPILER),gfortran)
 LAPACK_LIBS=-L$(MKLROOT)/lib/intel64 -Wl,-rpath=$(MKLROOT)/lib -lmkl_gf_$(ABI) -lmkl_sequential -lmkl_core -lpthread -lm -ldl $(shell if [ -L /usr/lib64/libmemkind.so ]; then echo '-lmemkind'; fi)
@@ -6,6 +8,6 @@ else # Intel compilers
 LAPACK_LIBS=-L$(MKLROOT)/lib/intel64 -Wl,-rpath=$(MKLROOT)/lib -lmkl_intel_$(ABI) -lmkl_sequential -lmkl_core -lpthread -lm -ldl $(shell if [ -L /usr/lib64/libmemkind.so ]; then echo '-lmemkind'; fi)
 endif # ?gfortran
 else # !x86_64
-LAPACK_LIBS=-L$(LAPACK) -ltmglib -llapack -lrefblas -lm
+# TODO
 endif # ?x86_64
-endif # !LAPACK
+endif # ?LAPACK
