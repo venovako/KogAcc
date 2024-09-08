2697ab1ef5884e81b560a2d71cfeefe5075c5d37
Vedran Novakovic <venovako@venovako.eu>
2024-09-08 22:15:37 +0200

    further updates

--- src/debug.mk
+++ src/debug.mk
@@ -1,29 +1,13 @@
 ifdef NDEBUG
 DEBUG=
-FCFLAGS += -DNDEBUG=$(NDEBUG)
+FCFLAGS += -DNDEBUG=$(NDEBUG) -DCR_MATH=bundled_noerrno
 else # !NDEBUG
 DEBUG=g
+FCFLAGS += -DCR_MATH=bundled
 endif # ?NDEBUG
 ifdef ANIMATE
 FCFLAGS += -DANIMATE=$(ANIMATE)
 endif # ANIMATE
-ifdef CR_MATH
-FCFLAGS += -DCR_MATH=$(CR_MATH)
-ifdef NDEBUG
-# modified routines that do not reference errno
-OBJS_CRF_MATH=$(CR_MATH)/src/binary32/hypot/hypotf_noerrno.o
-OBJS_CRD_MATH=$(CR_MATH)/src/binary64/hypot/hypot_noerrno.o
-else # DEBUG
-OBJS_CRF_MATH=$(CR_MATH)/src/binary32/hypot/hypotf.o
-OBJS_CRD_MATH=$(CR_MATH)/src/binary64/hypot/hypot.o
-endif # ?NDEBUG
-else # !CR_MATH
-ifdef NDEBUG
-FCFLAGS += -DCR_MATH=bundled_noerrno
-else # !NDEBUG
-FCFLAGS += -DCR_MATH=bundled
-endif # ?NDEBUG
-endif # ?CR_MATH
 ifdef PROFILE
 FCFLAGS += -DPVN_PROFILE=$(PROFILE)u -fno-inline -finstrument-functions
 endif # PROFILE
