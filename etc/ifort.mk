0c3fc81b847dac8ff3e50fd13e14fc7deba1a525
Vedran Novakovic <venovako@venovako.eu>
2024-09-17 03:17:52 +0200

    further updates

--- etc/ifort.mk
+++ etc/ifort.mk
@@ -7,7 +7,11 @@ FCFLAGS=-O$(NDEBUG)
 else # !NDEBUG
 FCFLAGS=-O0 -g
 endif # ?NDEBUG
-FCFLAGS += -diag-disable=10397,10448 -xHost -fPIC -fexceptions -fno-omit-frame-pointer -qopt-multi-version-aggressive -qopt-zmm-usage=high -fp-model precise -fma -fprotect-parens -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -qopenmp -qsimd-honor-fp-model -qsimd-serialize-fp-reduction -standard-semantics -traceback -vec-threshold0 -rdynamic # use -xCOMMON-AVX512 for Intel Xeon Phis
+ifndef CPU
+CPU=Host
+# COMMON-AVX512 for KNLs
+endif # !CPU
+FCFLAGS += -diag-disable=10397,10448,10441 -x$(CPU) -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -qopt-multi-version-aggressive -qopt-zmm-usage=high -fp-model precise -fma -fprotect-parens -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -qopenmp -qsimd-honor-fp-model -qsimd-serialize-fp-reduction -standard-semantics -traceback -vec-threshold0 -rdynamic
 ifdef NDEBUG
 FCFLAGS += -qopt-report=5 -inline-level=2
 else # !NDEBUG
