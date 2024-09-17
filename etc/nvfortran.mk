5730557641de7d44b5d6b060c13d669cc13c2315
Vedran Novakovic <venovako@venovako.eu>
2024-09-17 23:07:00 +0200

    further updates

--- etc/nvfortran.mk
+++ etc/nvfortran.mk
@@ -6,7 +6,13 @@ FCFLAGS=-O$(NDEBUG)
 else # !NDEBUG
 FCFLAGS=-O0 -g
 endif # ?NDEBUG
-FCFLAGS += -mp -KPIC -Mframe -Meh_frame -Minfo -Mdclchk -Mlarge_arrays -Mrecursive -Mstack_arrays -Kieee -Mfma -Mnodaz -Mnoflushz -Mnofpapprox -Mnofprelaxed -tp=native -traceback
-ifndef NDEBUG
-FCFLAGS += -Mbounds -Mchkstk
-endif # !NDEBUG
+ifndef CPU
+CPU=native
+endif # !CPU
+FCFLAGS += -m64 -mp -KPIC -Mdclchk -Mframe -Meh_frame -Minfo -Mlarge_arrays -Mrecursive -Mstack_arrays -tp=$(CPU) -Kieee -Mfma -Mnodaz -Mnoflushz -Mnofpapprox -Mnofprelaxed -Mno-recip-div -nvmalloc -traceback
+ifdef NDEBUG
+FCFLAGS += -O$(NDEBUG)
+else # !NDEBUG
+FCFLAGS += -O0 -g -Mbounds -Mchkstk
+endif # ?NDEBUG
+FCFLAGS += -Wl,-E
