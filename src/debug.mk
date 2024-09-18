bdd8e15018b93a2d38c097388c43726841c02d18
Vedran Novakovic <venovako@venovako.eu>
2024-09-18 20:16:11 +0200

    further updates

--- src/debug.mk
+++ src/debug.mk
@@ -7,9 +7,11 @@ FCFLAGS += -DCR_MATH=bundled
 endif # ?NDEBUG
 ifdef ANIMATE
 FCFLAGS += -DANIMATE=$(ANIMATE)
+ifeq ($(findstring 86,$(ARCH)),86)
 ifneq ($(COMPILER),gfortran)
 GNU=$(realpath $(shell gcc -print-file-name=libquadmath.a))
 endif # !gfortran
+endif # 86
 endif # ANIMATE
 ifdef PROFILE
 FCFLAGS += -DPVN_PROFILE=$(PROFILE)u -fno-inline -finstrument-functions
