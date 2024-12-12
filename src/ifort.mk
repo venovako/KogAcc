2fa865ee7a412606bf3c732399aed93dd2f52094
Vedran Novakovic <venovako@venovako.eu>
2024-12-12 04:25:59 +0100

    further updates

--- src/ifort.mk
+++ src/ifort.mk
@@ -11,7 +11,12 @@ ifndef MARCH
 MARCH=Host
 # COMMON-AVX512 for KNLs
 endif # !MARCH
-FCFLAGS += -diag-disable=10397,10448,10441 -x$(MARCH) -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -qopt-multi-version-aggressive -qopt-zmm-usage=high -fp-model precise -fma -fprotect-parens -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -qopenmp -qsimd-honor-fp-model -qsimd-serialize-fp-reduction -standard-semantics -traceback -vec-threshold0 -rdynamic
+ifeq ($(ABI),ilp64)
+FCFLAGS += -qmkl-$(ABI)=sequential
+else # !ilp64
+FCFLAGS += -qmkl=sequential
+endif # ?ilp64
+FCFLAGS += -diag-disable=10397,10448,10441 -x$(MARCH) -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -qopt-multi-version-aggressive -qopt-zmm-usage=high -fp-model precise -fma -fprotect-parens -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -qopenmp -qsimd-honor-fp-model -qsimd-serialize-fp-reduction -standard-semantics -traceback -vec-threshold0 -rdynamic -static-intel
 ifdef NDEBUG
 FCFLAGS += -fno-math-errno -qopt-report=5
 ifndef PROFILE
