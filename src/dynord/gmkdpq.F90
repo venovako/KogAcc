d88d1cc905e22c8f98fc8df933cc18b63365c6d1
Vedran Novakovic <venovako@venovako.eu>
2024-12-27 02:18:50 +0100

    further updates

--- src/dynord/gmkdpq.F90
+++ src/dynord/gmkdpq.F90
@@ -10,25 +10,55 @@
   IF (W .LE. WZERO) RETURN
 
   R = N / 2
-  DO INFO = 1, R
-     P = 0; Q = 0
-     CALL XDEC(W, P, Q)
-     O(1,INFO) = P
-     O(2,INFO) = Q
-     IF (INFO .GE. R) EXIT
-     W = WZERO
-     !$OMP PARALLEL DO DEFAULT(NONE) SHARED(D,M,P,Q) PRIVATE(I,J,K) REDUCTION(MAX:W) IF(L .NE. 0)
-     DO K = 1, M
-        IF (D(K) .GT. WZERO) THEN
-           I = 0; J = 0
-           CALL XDEC(D(K), I, J)
-           IF ((I .NE. P) .AND. (I .NE. Q) .AND. (J .NE. P) .AND. (J .NE. Q)) THEN
-              W = MAX(W, D(K))
-           ELSE ! colliding
-              D(K) = -D(K)
+  IF (L .EQ. 0) THEN
+     L = M
+     DO INFO = 1, R
+        P = 0; Q = 0
+        CALL XDEC(W, P, Q)
+        O(1,INFO) = P
+        O(2,INFO) = Q
+        IF (INFO .GE. R) EXIT
+        W = WZERO
+        K = 1
+        DO WHILE (K .LE. L)
+           IF (D(K) .GT. WZERO) THEN
+              I = 0; J = 0
+              CALL XDEC(D(K), I, J)
+              IF ((I .NE. P) .AND. (I .NE. Q) .AND. (J .NE. P) .AND. (J .NE. Q)) THEN
+                 W = MAX(W, D(K))
+                 K = K + 1
+              ELSE ! colliding
+                 D(K) = D(L)
+                 L = L - 1
+              END IF
+           ELSE ! can be skipped
+              D(K) = D(L)
+              L = L - 1
            END IF
-        END IF
+        END DO
+        IF (W .LE. WZERO) EXIT
      END DO
-     !$OMP END PARALLEL DO
-     IF (W .LE. WZERO) EXIT
-  END DO
+  ELSE ! L .NE. 0
+     DO INFO = 1, R
+        P = 0; Q = 0
+        CALL XDEC(W, P, Q)
+        O(1,INFO) = P
+        O(2,INFO) = Q
+        IF (INFO .GE. R) EXIT
+        W = WZERO
+        !$OMP PARALLEL DO DEFAULT(NONE) SHARED(D,M,P,Q) PRIVATE(I,J,K) REDUCTION(MAX:W)
+        DO K = 1, M
+           IF (D(K) .GT. WZERO) THEN
+              I = 0; J = 0
+              CALL XDEC(D(K), I, J)
+              IF ((I .NE. P) .AND. (I .NE. Q) .AND. (J .NE. P) .AND. (J .NE. Q)) THEN
+                 W = MAX(W, D(K))
+              ELSE ! colliding
+                 D(K) = -D(K)
+              END IF
+           END IF
+        END DO
+        !$OMP END PARALLEL DO
+        IF (W .LE. WZERO) EXIT
+     END DO
+  END IF
