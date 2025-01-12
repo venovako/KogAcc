3cb5e58de9c862d6d1cf015457f18268ab5f4129
Vedran Novakovic <venovako@venovako.eu>
2025-01-12 19:42:20 +0100

    further updates

--- etc/sh/db2.sh
+++ etc/sh/db2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 source ${HOME}/Downloads/KogAcc/etc/env.sh 2
-hostname > db$1-$2.out
-date > db$1-$2.err
+hostname > db$1-$2-$4.out
+date > db$1-$2-$4.err
 let "T=$3/$4"
 echo "$3 threads split to ${T} outer and $4 inner threads."
 for ((I=128;I<=5376;I+=128))
 do
-	printf "%4d " $I >> db$1-$2.out
-	OMP_NUM_THREADS="${T},$4" ${HOME}/Downloads/KogAcc/bin/ifx/Linux-x86_64-lp64/dksvd1.exe $1 ${I} $2 dR${I} >> db$1-$2.out 2>> db$1-$2.err
+	printf "%4d " $I >> db$1-$2-$4.out
+	OMP_NUM_THREADS="${T},$4" ${HOME}/Downloads/KogAcc/bin/ifx/Linux-x86_64-lp64/dksvd1.exe $1 ${I} $2 dR${I} >> db$1-$2-$4.out 2>> db$1-$2-$4.err
 done
-echo '"N", "TIMEs", "PSTEPS", "SVDRES", "UORTHO", "VORTHO", "MINAE", "MAXAE", "AVGAE", "MINRE", "MAXRE", "AVGRE"' > db$1-$2.csv
+echo '"N", "TIMEs", "PSTEPS", "SVDRES", "UORTHO", "VORTHO", "MINAE", "MAXAE", "AVGAE", "MINRE", "MAXRE", "AVGRE"' > db$1-$2-$4.csv
 for ((I=128;I<=5376;I+=128))
 do
 	B=dR${I}
@@ -17,7 +17,7 @@ do
 	U=`OMP_NUM_THREADS=$3 ${HOME}/Downloads/JACSD/tortho/dortho.exe ${B}.U$1 ${I} ${I}`
 	V=`OMP_NUM_THREADS=$3 ${HOME}/Downloads/JACSD/tortho/dortho.exe ${B}.V$1 ${I} ${I}`
 	S=`${HOME}/Downloads/JACSD/tgensvd/dsverr.exe ${I} ${B}.S ${B}.S$1`
-	printf "%4d," $I >> db$1-$2.csv
-	echo "${A},${U},${V},${S}" >> db$1-$2.csv
+	printf "%4d," $I >> db$1-$2-$4.csv
+	echo "${A},${U},${V},${S}" >> db$1-$2-$4.csv
 done
 unset I
--- etc/sh/sb2.sh
+++ etc/sh/sb2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 source ${HOME}/Downloads/KogAcc/etc/env.sh 2
-hostname > sb$1-$2.out
-date > sb$1-$2.err
+hostname > sb$1-$2-$4.out
+date > sb$1-$2-$4.err
 let "T=$3/$4"
 echo "$3 threads split to ${T} outer and $4 inner threads."
 for ((I=128;I<=5376;I+=128))
 do
-	printf "%4d " $I >> sb$1-$2.out
-	OMP_NUM_THREADS="${T},$4" ${HOME}/Downloads/KogAcc/bin/ifx/Linux-x86_64-lp64/sksvd1.exe $1 ${I} $2 s${I} >> sb$1-$2.out 2>> sb$1-$2.err
+	printf "%4d " $I >> sb$1-$2-$4.out
+	OMP_NUM_THREADS="${T},$4" ${HOME}/Downloads/KogAcc/bin/ifx/Linux-x86_64-lp64/sksvd1.exe $1 ${I} $2 s${I} >> sb$1-$2-$4.out 2>> sb$1-$2-$4.err
 done
-echo '"N", "TIMEs", "PSTEPS", "SVDRES", "UORTHO", "VORTHO", "MINAE", "MAXAE", "AVGAE", "MINRE", "MAXRE", "AVGRE"' > sb$1-$2.csv
+echo '"N", "TIMEs", "PSTEPS", "SVDRES", "UORTHO", "VORTHO", "MINAE", "MAXAE", "AVGAE", "MINRE", "MAXRE", "AVGRE"' > sb$1-$2-$4.csv
 for ((I=128;I<=5376;I+=128))
 do
 	B=s${I}
@@ -17,7 +17,7 @@ do
 	U=`OMP_NUM_THREADS=$3 ${HOME}/Downloads/JACSD/tortho/sortho.exe ${B}.U$1 ${I} ${I}`
 	V=`OMP_NUM_THREADS=$3 ${HOME}/Downloads/JACSD/tortho/sortho.exe ${B}.V$1 ${I} ${I}`
 	S=`${HOME}/Downloads/JACSD/tgensvd/ssverr.exe ${I} ${B}.S ${B}.S$1`
-	printf "%4d," $I >> sb$1-$2.csv
-	echo "${A},${U},${V},${S}" >> sb$1-$2.csv
+	printf "%4d," $I >> sb$1-$2-$4.csv
+	echo "${A},${U},${V},${S}" >> sb$1-$2-$4.csv
 done
 unset I
