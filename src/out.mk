6343e4444a988931875995d93ccb1ed505b7c1ce
Vedran Novakovic <venovako@venovako.eu>
2025-04-21 03:45:53 +0200

    further updates

--- src/out.mk
+++ src/out.mk
@@ -1,241 +1,238 @@
-OBJS_ARRBIO=\
-..\obj\$(PLAT)\bfopen.obj\
-..\obj\$(PLAT)\ibrd1.obj\
-..\obj\$(PLAT)\jbrd1.obj\
-..\obj\$(PLAT)\sbrd1.obj\
-..\obj\$(PLAT)\cbrd1.obj\
-..\obj\$(PLAT)\dbrd1.obj\
-..\obj\$(PLAT)\zbrd1.obj\
-..\obj\$(PLAT)\ibwr1.obj\
-..\obj\$(PLAT)\jbwr1.obj\
-..\obj\$(PLAT)\sbwr1.obj\
-..\obj\$(PLAT)\cbwr1.obj\
-..\obj\$(PLAT)\dbwr1.obj\
-..\obj\$(PLAT)\zbwr1.obj\
-..\obj\$(PLAT)\ibrd2.obj\
-..\obj\$(PLAT)\jbrd2.obj\
-..\obj\$(PLAT)\sbrd2.obj\
-..\obj\$(PLAT)\cbrd2.obj\
-..\obj\$(PLAT)\dbrd2.obj\
-..\obj\$(PLAT)\zbrd2.obj\
-..\obj\$(PLAT)\ibwr2.obj\
-..\obj\$(PLAT)\jbwr2.obj\
-..\obj\$(PLAT)\sbwr2.obj\
-..\obj\$(PLAT)\cbwr2.obj\
-..\obj\$(PLAT)\dbwr2.obj\
+OBJS_ARRBIO= \
+..\obj\$(PLAT)\bfopen.obj \
+..\obj\$(PLAT)\ibrd1.obj  \
+..\obj\$(PLAT)\jbrd1.obj  \
+..\obj\$(PLAT)\sbrd1.obj  \
+..\obj\$(PLAT)\cbrd1.obj  \
+..\obj\$(PLAT)\dbrd1.obj  \
+..\obj\$(PLAT)\zbrd1.obj  \
+..\obj\$(PLAT)\ibwr1.obj  \
+..\obj\$(PLAT)\jbwr1.obj  \
+..\obj\$(PLAT)\sbwr1.obj  \
+..\obj\$(PLAT)\cbwr1.obj  \
+..\obj\$(PLAT)\dbwr1.obj  \
+..\obj\$(PLAT)\zbwr1.obj  \
+..\obj\$(PLAT)\ibrd2.obj  \
+..\obj\$(PLAT)\jbrd2.obj  \
+..\obj\$(PLAT)\sbrd2.obj  \
+..\obj\$(PLAT)\cbrd2.obj  \
+..\obj\$(PLAT)\dbrd2.obj  \
+..\obj\$(PLAT)\zbrd2.obj  \
+..\obj\$(PLAT)\ibwr2.obj  \
+..\obj\$(PLAT)\jbwr2.obj  \
+..\obj\$(PLAT)\sbwr2.obj  \
+..\obj\$(PLAT)\cbwr2.obj  \
+..\obj\$(PLAT)\dbwr2.obj  \
 ..\obj\$(PLAT)\zbwr2.obj
 
-OBJS_BLKSVD=\
-..\obj\$(PLAT)\nb2m.obj\
-..\obj\$(PLAT)\sbrdg.obj\
-..\obj\$(PLAT)\cbrdg.obj\
-..\obj\$(PLAT)\dbrdg.obj\
-..\obj\$(PLAT)\zbrdg.obj\
-..\obj\$(PLAT)\sbpack.obj\
-..\obj\$(PLAT)\cbpack.obj\
-..\obj\$(PLAT)\dbpack.obj\
-..\obj\$(PLAT)\zbpack.obj\
-..\obj\$(PLAT)\sbunpack.obj\
-..\obj\$(PLAT)\cbunpack.obj\
-..\obj\$(PLAT)\dbunpack.obj\
-..\obj\$(PLAT)\zbunpack.obj\
-..\obj\$(PLAT)\sbupdate.obj\
-..\obj\$(PLAT)\cbupdate.obj\
-..\obj\$(PLAT)\dbupdate.obj\
-..\obj\$(PLAT)\zbupdate.obj\
-..\obj\$(PLAT)\sksvd0.obj\
-..\obj\$(PLAT)\cksvd0.obj\
-..\obj\$(PLAT)\dksvd0.obj\
-..\obj\$(PLAT)\zksvd0.obj\
-..\obj\$(PLAT)\sksvd1.obj\
-..\obj\$(PLAT)\cksvd1.obj\
-..\obj\$(PLAT)\dksvd1.obj\
-..\obj\$(PLAT)\zksvd1.obj\
-..\obj\$(PLAT)\sksvdd.obj\
-..\obj\$(PLAT)\dksvdd.obj\
-..\obj\$(PLAT)\cksvdd.obj\
-..\obj\$(PLAT)\zksvdd.obj\
-..\obj\$(PLAT)\sbksvd0.obj\
-..\obj\$(PLAT)\cbksvd0.obj\
-..\obj\$(PLAT)\dbksvd0.obj\
-..\obj\$(PLAT)\zbksvd0.obj\
-..\obj\$(PLAT)\sbksvdd.obj\
-..\obj\$(PLAT)\cbksvdd.obj\
+OBJS_BLKSVD= \
+..\obj\$(PLAT)\nb2m.obj     \
+..\obj\$(PLAT)\sbrdg.obj    \
+..\obj\$(PLAT)\cbrdg.obj    \
+..\obj\$(PLAT)\dbrdg.obj    \
+..\obj\$(PLAT)\zbrdg.obj    \
+..\obj\$(PLAT)\sbpack.obj   \
+..\obj\$(PLAT)\cbpack.obj   \
+..\obj\$(PLAT)\dbpack.obj   \
+..\obj\$(PLAT)\zbpack.obj   \
+..\obj\$(PLAT)\sbunpack.obj \
+..\obj\$(PLAT)\cbunpack.obj \
+..\obj\$(PLAT)\dbunpack.obj \
+..\obj\$(PLAT)\zbunpack.obj \
+..\obj\$(PLAT)\sbupdate.obj \
+..\obj\$(PLAT)\cbupdate.obj \
+..\obj\$(PLAT)\dbupdate.obj \
+..\obj\$(PLAT)\zbupdate.obj \
+..\obj\$(PLAT)\sksvd0.obj   \
+..\obj\$(PLAT)\cksvd0.obj   \
+..\obj\$(PLAT)\dksvd0.obj   \
+..\obj\$(PLAT)\zksvd0.obj   \
+..\obj\$(PLAT)\sksvd1.obj   \
+..\obj\$(PLAT)\cksvd1.obj   \
+..\obj\$(PLAT)\dksvd1.obj   \
+..\obj\$(PLAT)\zksvd1.obj   \
+..\obj\$(PLAT)\sksvdd.obj   \
+..\obj\$(PLAT)\dksvdd.obj   \
+..\obj\$(PLAT)\cksvdd.obj   \
+..\obj\$(PLAT)\zksvdd.obj   \
+..\obj\$(PLAT)\sbksvd0.obj  \
+..\obj\$(PLAT)\cbksvd0.obj  \
+..\obj\$(PLAT)\dbksvd0.obj  \
+..\obj\$(PLAT)\zbksvd0.obj  \
+..\obj\$(PLAT)\sbksvdd.obj  \
+..\obj\$(PLAT)\cbksvdd.obj  \
 ..\obj\$(PLAT)\ibdims.obj
 
-OBJS_DYNORD=\
-..\obj\$(PLAT)\snrm2o.obj\
-..\obj\$(PLAT)\cnrm2o.obj\
-..\obj\$(PLAT)\dnrm2o.obj\
-..\obj\$(PLAT)\znrm2o.obj\
-..\obj\$(PLAT)\snrm2b.obj\
-..\obj\$(PLAT)\cnrm2b.obj\
-..\obj\$(PLAT)\dnrm2b.obj\
-..\obj\$(PLAT)\znrm2b.obj\
-..\obj\$(PLAT)\sb2enc.obj\
-..\obj\$(PLAT)\db2enc.obj\
-..\obj\$(PLAT)\smkd.obj\
-..\obj\$(PLAT)\dmkd.obj\
-..\obj\$(PLAT)\cmkd.obj\
-..\obj\$(PLAT)\zmkd.obj\
-..\obj\$(PLAT)\smkdpq.obj\
-..\obj\$(PLAT)\dmkdpq.obj\
-..\obj\$(PLAT)\smkbpq.obj\
-..\obj\$(PLAT)\dmkbpq.obj\
-..\obj\$(PLAT)\cmkbpq.obj\
+OBJS_DYNORD= \
+..\obj\$(PLAT)\snrm2o.obj \
+..\obj\$(PLAT)\cnrm2o.obj \
+..\obj\$(PLAT)\dnrm2o.obj \
+..\obj\$(PLAT)\znrm2o.obj \
+..\obj\$(PLAT)\snrm2b.obj \
+..\obj\$(PLAT)\cnrm2b.obj \
+..\obj\$(PLAT)\dnrm2b.obj \
+..\obj\$(PLAT)\znrm2b.obj \
+..\obj\$(PLAT)\sb2enc.obj \
+..\obj\$(PLAT)\db2enc.obj \
+..\obj\$(PLAT)\smkd.obj   \
+..\obj\$(PLAT)\dmkd.obj   \
+..\obj\$(PLAT)\cmkd.obj   \
+..\obj\$(PLAT)\zmkd.obj   \
+..\obj\$(PLAT)\smkdpq.obj \
+..\obj\$(PLAT)\dmkdpq.obj \
+..\obj\$(PLAT)\smkbpq.obj \
+..\obj\$(PLAT)\dmkbpq.obj \
+..\obj\$(PLAT)\cmkbpq.obj \
 ..\obj\$(PLAT)\zmkbpq.obj
 
-OBJS_KSVD2=\
-..\obj\$(PLAT)\sksvd2.obj\
-..\obj\$(PLAT)\cksvd2.obj\
-..\obj\$(PLAT)\dksvd2.obj\
-..\obj\$(PLAT)\zksvd2.obj\
-..\obj\$(PLAT)\qksvd2.obj\
-..\obj\$(PLAT)\yksvd2.obj\
-..\obj\$(PLAT)\scvgpp.obj\
-..\obj\$(PLAT)\ccvgpp.obj\
-..\obj\$(PLAT)\dcvgpp.obj\
-..\obj\$(PLAT)\zcvgpp.obj\
-..\obj\$(PLAT)\slmsv2.obj\
-..\obj\$(PLAT)\dlmsv2.obj\
-..\obj\$(PLAT)\qlmsv2.obj\
-..\obj\$(PLAT)\slwsv2.obj\
-..\obj\$(PLAT)\dlwsv2.obj\
-..\obj\$(PLAT)\qlwsv2.obj\
-..\obj\$(PLAT)\skerr2.obj\
-..\obj\$(PLAT)\ckerr2.obj\
-..\obj\$(PLAT)\dkerr2.obj\
-..\obj\$(PLAT)\zkerr2.obj\
-..\obj\$(PLAT)\qkerr2.obj\
+OBJS_KSVD2= \
+..\obj\$(PLAT)\sksvd2.obj \
+..\obj\$(PLAT)\cksvd2.obj \
+..\obj\$(PLAT)\dksvd2.obj \
+..\obj\$(PLAT)\zksvd2.obj \
+..\obj\$(PLAT)\qksvd2.obj \
+..\obj\$(PLAT)\yksvd2.obj \
+..\obj\$(PLAT)\scvgpp.obj \
+..\obj\$(PLAT)\ccvgpp.obj \
+..\obj\$(PLAT)\dcvgpp.obj \
+..\obj\$(PLAT)\zcvgpp.obj \
+..\obj\$(PLAT)\slmsv2.obj \
+..\obj\$(PLAT)\dlmsv2.obj \
+..\obj\$(PLAT)\qlmsv2.obj \
+..\obj\$(PLAT)\slwsv2.obj \
+..\obj\$(PLAT)\dlwsv2.obj \
+..\obj\$(PLAT)\qlwsv2.obj \
+..\obj\$(PLAT)\skerr2.obj \
+..\obj\$(PLAT)\ckerr2.obj \
+..\obj\$(PLAT)\dkerr2.obj \
+..\obj\$(PLAT)\zkerr2.obj \
+..\obj\$(PLAT)\qkerr2.obj \
 ..\obj\$(PLAT)\ykerr2.obj
 
-OBJS_FAUX=\
-..\obj\$(PLAT)\sthalt.obj\
-..\obj\$(PLAT)\cmul.obj\
-..\obj\$(PLAT)\zmul.obj\
-..\obj\$(PLAT)\cfma.obj\
-..\obj\$(PLAT)\zfma.obj\
-..\obj\$(PLAT)\sconjt.obj\
-..\obj\$(PLAT)\cconjt.obj\
-..\obj\$(PLAT)\dconjt.obj\
-..\obj\$(PLAT)\zconjt.obj\
-..\obj\$(PLAT)\slango.obj\
-..\obj\$(PLAT)\clango.obj\
-..\obj\$(PLAT)\dlango.obj\
-..\obj\$(PLAT)\zlango.obj\
-..\obj\$(PLAT)\srotc.obj\
-..\obj\$(PLAT)\srotca.obj\
-..\obj\$(PLAT)\crotc.obj\
-..\obj\$(PLAT)\crotca.obj\
-..\obj\$(PLAT)\drotc.obj\
-..\obj\$(PLAT)\drotca.obj\
-..\obj\$(PLAT)\zrotc.obj\
-..\obj\$(PLAT)\zrotca.obj\
-..\obj\$(PLAT)\sbrotc.obj\
-..\obj\$(PLAT)\cbrotc.obj\
-..\obj\$(PLAT)\dbrotc.obj\
-..\obj\$(PLAT)\zbrotc.obj\
-..\obj\$(PLAT)\srotr.obj\
-..\obj\$(PLAT)\srotra.obj\
-..\obj\$(PLAT)\crotr.obj\
-..\obj\$(PLAT)\crotra.obj\
-..\obj\$(PLAT)\drotr.obj\
-..\obj\$(PLAT)\drotra.obj\
-..\obj\$(PLAT)\zrotr.obj\
-..\obj\$(PLAT)\zrotra.obj\
-..\obj\$(PLAT)\sbrotr.obj\
-..\obj\$(PLAT)\cbrotr.obj\
-..\obj\$(PLAT)\dbrotr.obj\
-..\obj\$(PLAT)\zbrotr.obj\
-..\obj\$(PLAT)\sscalg.obj\
-..\obj\$(PLAT)\cscalg.obj\
-..\obj\$(PLAT)\dscalg.obj\
-..\obj\$(PLAT)\zscalg.obj\
-..\obj\$(PLAT)\initrc.obj\
-..\obj\$(PLAT)\initcc.obj\
-..\obj\$(PLAT)\initme.obj\
-..\obj\$(PLAT)\initmm.obj\
-..\obj\$(PLAT)\jstep.obj\
-..\obj\$(PLAT)\jsweep.obj
-
-ifndef LAPACK
-OBJS_FAUX += \
-..\obj\$(PLAT)\scp.obj\
-..\obj\$(PLAT)\ccp.obj\
-..\obj\$(PLAT)\dcp.obj\
-..\obj\$(PLAT)\zcp.obj\
-..\obj\$(PLAT)\smm.obj\
-..\obj\$(PLAT)\cmm.obj\
-..\obj\$(PLAT)\dmm.obj\
+OBJS_FAUX= \
+..\obj\$(PLAT)\sthalt.obj \
+..\obj\$(PLAT)\cmul.obj   \
+..\obj\$(PLAT)\zmul.obj   \
+..\obj\$(PLAT)\cfma.obj   \
+..\obj\$(PLAT)\zfma.obj   \
+..\obj\$(PLAT)\sconjt.obj \
+..\obj\$(PLAT)\cconjt.obj \
+..\obj\$(PLAT)\dconjt.obj \
+..\obj\$(PLAT)\zconjt.obj \
+..\obj\$(PLAT)\slango.obj \
+..\obj\$(PLAT)\clango.obj \
+..\obj\$(PLAT)\dlango.obj \
+..\obj\$(PLAT)\zlango.obj \
+..\obj\$(PLAT)\srotc.obj  \
+..\obj\$(PLAT)\srotca.obj \
+..\obj\$(PLAT)\crotc.obj  \
+..\obj\$(PLAT)\crotca.obj \
+..\obj\$(PLAT)\drotc.obj  \
+..\obj\$(PLAT)\drotca.obj \
+..\obj\$(PLAT)\zrotc.obj  \
+..\obj\$(PLAT)\zrotca.obj \
+..\obj\$(PLAT)\sbrotc.obj \
+..\obj\$(PLAT)\cbrotc.obj \
+..\obj\$(PLAT)\dbrotc.obj \
+..\obj\$(PLAT)\zbrotc.obj \
+..\obj\$(PLAT)\srotr.obj  \
+..\obj\$(PLAT)\srotra.obj \
+..\obj\$(PLAT)\crotr.obj  \
+..\obj\$(PLAT)\crotra.obj \
+..\obj\$(PLAT)\drotr.obj  \
+..\obj\$(PLAT)\drotra.obj \
+..\obj\$(PLAT)\zrotr.obj  \
+..\obj\$(PLAT)\zrotra.obj \
+..\obj\$(PLAT)\sbrotr.obj \
+..\obj\$(PLAT)\cbrotr.obj \
+..\obj\$(PLAT)\dbrotr.obj \
+..\obj\$(PLAT)\zbrotr.obj \
+..\obj\$(PLAT)\sscalg.obj \
+..\obj\$(PLAT)\cscalg.obj \
+..\obj\$(PLAT)\dscalg.obj \
+..\obj\$(PLAT)\zscalg.obj \
+..\obj\$(PLAT)\initrc.obj \
+..\obj\$(PLAT)\initcc.obj \
+..\obj\$(PLAT)\initme.obj \
+..\obj\$(PLAT)\initmm.obj \
+..\obj\$(PLAT)\jstep.obj  \
+..\obj\$(PLAT)\jsweep.obj \
+..\obj\$(PLAT)\scp.obj    \
+..\obj\$(PLAT)\ccp.obj    \
+..\obj\$(PLAT)\dcp.obj    \
+..\obj\$(PLAT)\zcp.obj    \
+..\obj\$(PLAT)\smm.obj    \
+..\obj\$(PLAT)\cmm.obj    \
+..\obj\$(PLAT)\dmm.obj    \
 ..\obj\$(PLAT)\zmm.obj
-endif
 
-EXES_BLKSVD=\
-..\bin\$(PLAT)\sksvd0.exe\
-..\bin\$(PLAT)\cksvd0.exe\
-..\bin\$(PLAT)\dksvd0.exe\
-..\bin\$(PLAT)\zksvd0.exe\
-..\bin\$(PLAT)\sksvd1.exe\
-..\bin\$(PLAT)\cksvd1.exe\
-..\bin\$(PLAT)\dksvd1.exe\
-..\bin\$(PLAT)\zksvd1.exe\
-..\bin\$(PLAT)\sksvdd.exe\
-..\bin\$(PLAT)\dksvdd.exe\
-..\bin\$(PLAT)\cksvdd.exe\
-..\bin\$(PLAT)\zksvdd.exe\
+
+EXES_BLKSVD= \
+..\bin\$(PLAT)\sksvd0.exe \
+..\bin\$(PLAT)\cksvd0.exe \
+..\bin\$(PLAT)\dksvd0.exe \
+..\bin\$(PLAT)\zksvd0.exe \
+..\bin\$(PLAT)\sksvd1.exe \
+..\bin\$(PLAT)\cksvd1.exe \
+..\bin\$(PLAT)\dksvd1.exe \
+..\bin\$(PLAT)\zksvd1.exe \
+..\bin\$(PLAT)\sksvdd.exe \
+..\bin\$(PLAT)\dksvdd.exe \
+..\bin\$(PLAT)\cksvdd.exe \
+..\bin\$(PLAT)\zksvdd.exe \
 ..\bin\$(PLAT)\ibdims.exe
 
-EXES_DYNORD=\
-..\bin\$(PLAT)\smkbpq.exe\
-..\bin\$(PLAT)\dmkbpq.exe\
-..\bin\$(PLAT)\cmkbpq.exe\
+EXES_DYNORD= \
+..\bin\$(PLAT)\smkbpq.exe \
+..\bin\$(PLAT)\dmkbpq.exe \
+..\bin\$(PLAT)\cmkbpq.exe \
 ..\bin\$(PLAT)\zmkbpq.exe
 
-EXES_KSVD2=\
-..\bin\$(PLAT)\sksvd2.exe\
-..\bin\$(PLAT)\cksvd2.exe\
-..\bin\$(PLAT)\dksvd2.exe\
-..\bin\$(PLAT)\zksvd2.exe\
-..\bin\$(PLAT)\qksvd2.exe\
-..\bin\$(PLAT)\yksvd2.exe\
-..\bin\$(PLAT)\slwsv2.exe\
-..\bin\$(PLAT)\dlwsv2.exe\
+EXES_KSVD2= \
+..\bin\$(PLAT)\sksvd2.exe \
+..\bin\$(PLAT)\cksvd2.exe \
+..\bin\$(PLAT)\dksvd2.exe \
+..\bin\$(PLAT)\zksvd2.exe \
+..\bin\$(PLAT)\qksvd2.exe \
+..\bin\$(PLAT)\yksvd2.exe \
+..\bin\$(PLAT)\slwsv2.exe \
+..\bin\$(PLAT)\dlwsv2.exe \
 ..\bin\$(PLAT)\qlwsv2.exe
 
-EXES_FAUX=\
-..\bin\$(PLAT)\sthalt.exe\
+EXES_FAUX= \
+..\bin\$(PLAT)\sthalt.exe \
 ..\bin\$(PLAT)\jsweep.exe
 
-EXES_TEST=\
-..\bin\$(PLAT)\sran.exe\
-..\bin\$(PLAT)\cran.exe\
-..\bin\$(PLAT)\dran.exe\
-..\bin\$(PLAT)\zran.exe\
-..\bin\$(PLAT)\srnd.exe\
-..\bin\$(PLAT)\crnd.exe\
-..\bin\$(PLAT)\drnd.exe\
-..\bin\$(PLAT)\zrnd.exe\
-..\bin\$(PLAT)\seedsz.exe\
-..\bin\$(PLAT)\sran2u.exe\
-..\bin\$(PLAT)\sran2g.exe\
-..\bin\$(PLAT)\cran2g.exe\
-..\bin\$(PLAT)\dran2u.exe\
-..\bin\$(PLAT)\dran2g.exe\
-..\bin\$(PLAT)\zran2g.exe\
-..\bin\$(PLAT)\srnd2u.exe\
-..\bin\$(PLAT)\srnd2g.exe\
-..\bin\$(PLAT)\crnd2g.exe\
-..\bin\$(PLAT)\drnd2u.exe\
-..\bin\$(PLAT)\drnd2g.exe\
+EXES_TEST= \
+..\bin\$(PLAT)\sran.exe   \
+..\bin\$(PLAT)\cran.exe   \
+..\bin\$(PLAT)\dran.exe   \
+..\bin\$(PLAT)\zran.exe   \
+..\bin\$(PLAT)\srnd.exe   \
+..\bin\$(PLAT)\crnd.exe   \
+..\bin\$(PLAT)\drnd.exe   \
+..\bin\$(PLAT)\zrnd.exe   \
+..\bin\$(PLAT)\seedsz.exe \
+..\bin\$(PLAT)\sran2u.exe \
+..\bin\$(PLAT)\sran2g.exe \
+..\bin\$(PLAT)\cran2g.exe \
+..\bin\$(PLAT)\dran2u.exe \
+..\bin\$(PLAT)\dran2g.exe \
+..\bin\$(PLAT)\zran2g.exe \
+..\bin\$(PLAT)\srnd2u.exe \
+..\bin\$(PLAT)\srnd2g.exe \
+..\bin\$(PLAT)\crnd2g.exe \
+..\bin\$(PLAT)\drnd2u.exe \
+..\bin\$(PLAT)\drnd2g.exe \
 ..\bin\$(PLAT)\zrnd2g.exe
 
 OBJS=$(OBJS_ARRBIO) $(OBJS_BLKSVD) $(OBJS_DYNORD) $(OBJS_KSVD2) $(OBJS_FAUX)
 
-LIBS=\
-..\lib\$(PLAT)\arrbio.lib\
-..\lib\$(PLAT)\blksvd.lib\
-..\lib\$(PLAT)\dynord.lib\
-..\lib\$(PLAT)\ksvd2.lib\
+LIBS= \
+..\lib\$(PLAT)\arrbio.lib \
+..\lib\$(PLAT)\blksvd.lib \
+..\lib\$(PLAT)\dynord.lib \
+..\lib\$(PLAT)\ksvd2.lib  \
 ..\lib\$(PLAT)\faux.lib
 
 EXES=$(EXES_BLKSVD) $(EXES_DYNORD) $(EXES_KSVD2) $(EXES_FAUX) $(EXES_TEST)
