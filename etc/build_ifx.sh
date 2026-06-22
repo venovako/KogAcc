b51f8f7ebfdbeb9f095113c0952e2c1acbcce242
Vedran Novakovic <venovako@venovako.eu>
2026-06-22 19:50:32 +0200

    further updates

--- etc/build_ifx.sh
+++ etc/build_ifx.sh
@@ -4,14 +4,14 @@ if [ -z "${TGT}" ]
 then
 	cd ../../libpvn/src
 	make COMPILER=icx NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 clean
-	make COMPILER=icx NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 -j all
+	make COMPILER=icx NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 -j
 	cd ../../KogAcc/src
 	make LAPACK=sequential clean all
 	cd ../etc
 else
 	cd ../../libpvn/src
-	make COMPILER=icx MARCH=${TGT} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 clean
-	make COMPILER=icx MARCH=${TGT} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 -j all
+	make COMPILER=icx NATIVE=haswell MARCH=${TGT} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 clean
+	make COMPILER=icx NATIVE=haswell MARCH=${TGT} NDEBUG=3 SAFE=DET,SV2,NRM OPENMP=0 -j
 	cd ../../KogAcc/src
 	make LAPACK=sequential clean all
 	cd ../etc
